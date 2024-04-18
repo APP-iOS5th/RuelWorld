fb969b1db1fc53762a84b8ddce7829fa82c8583e
Ruel <c0407h@gmail.com>
2024-04-18 13:35:54 +0900

    project init

---
+++ LAB7-Calculator/LAB7-Calculator/LAB7_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LAB7_CalculatorApp.swift
+//  LAB7-Calculator
+//
+//  Created by Chung Wussup on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct LAB7_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
