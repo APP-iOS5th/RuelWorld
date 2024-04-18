962bd2a464b4c896be9c2c3ea5c79fec375b3bea
youngwoo_ahn <codeway119@gmail.com>
2024-04-18 15:09:11 +0900

    layout

--- LAB7-Calculator/LAB7-Calculator/ContentView.swift
+++ LAB7-Calculator/LAB7-Calculator/ContentView.swift
@@ -7,24 +7,106 @@
 
 import SwiftUI
 
-struct NumberButtonView: View {
-    let number: Int
+struct textData {
+    
+}
+
+struct ButtonView: View {
+    
+    
+    let calChar: String
+    let buttonColor: Color
     var body: some View {
-        Button(number) {
-            { print(number) }
+        Button(calChar) {
+            print(calChar)
+        }
+        .frame(width: 80, height: 100)
+        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+        .background(buttonColor)
+    }
+}
+
+struct FullTextAreaView: View {
+    @Binding var calText: String
+    var body: some View {
+        Text("\(calText)")
+            .frame(width: 300, height: 70, alignment: .trailing)
+            .padding(.horizontal, 22)
+        
+            .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InputButtonAreaView: View {
+    let numberColor = Color.teal
+    let operationColor = Color.green
+    let actionColor  = Color.orange
+    var body: some View {
+        VStack {
+            HStack {
+                ButtonView(calChar: "7",
+                           buttonColor: numberColor)
+                ButtonView(calChar: "8",
+                           buttonColor: numberColor)
+                ButtonView(calChar: "9",
+                           buttonColor: numberColor)
+                ButtonView(calChar: "+",
+                           buttonColor: operationColor)
+            }
+            HStack {
+                ButtonView(calChar: "4",
+                           buttonColor: numberColor)
+                ButtonView(calChar: "5",
+                           buttonColor: numberColor)
+                ButtonView(calChar: "6",
+                           buttonColor: numberColor)
+                ButtonView(calChar: "-",
+                           buttonColor: operationColor)
+            }
+            HStack {
+                ButtonView(calChar: "1",
+                           buttonColor: numberColor)
+                ButtonView(calChar: "2",
+                           buttonColor: numberColor)
+                ButtonView(calChar: "3",
+                           buttonColor: numberColor)
+                ButtonView(calChar: "*",
+                           buttonColor: operationColor)
+            }
+            HStack {
+                ButtonView(calChar: "00",
+                           buttonColor: numberColor)
+                ButtonView(calChar: "0",
+                           buttonColor: numberColor)
+                ButtonView(calChar: ".",
+                           buttonColor: numberColor)
+                ButtonView(calChar: "/",
+                           buttonColor: operationColor)
+            }
+            HStack {
+                ButtonView(calChar: "C",
+                           buttonColor: actionColor)
+                ButtonView(calChar: "DEL",
+                           buttonColor: actionColor)
+                ButtonView(calChar: "=",
+                           buttonColor: actionColor)
+                ButtonView(calChar: "%",
+                           buttonColor: operationColor)
+            }
         }
     }
 }
 
 struct ContentView: View {
+    @State private var calText:String = "000"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world! youngwoo")
+            FullTextAreaView(calText: $calText)
+            InputButtonAreaView()
         }
         .padding()
+        .font(.largeTitle)
     }
 }
 
