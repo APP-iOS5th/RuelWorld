31f60f5f41440ad09bcb22983768d1a5a6aa281e
youngwoo_ahn <codeway119@gmail.com>
2024-04-18 17:24:14 +0900

    EnviromentObject

--- LAB7-Calculator/LAB7-Calculator/ContentView.swift
+++ LAB7-Calculator/LAB7-Calculator/ContentView.swift
@@ -7,18 +7,54 @@
 
 import SwiftUI
 
-struct textData {
+class SharedData: ObservableObject {
+    
+    @Published var calText: String = ""
+    
+    func buttonAction (calChar: String) {
+        calText += calChar
+        print(calText)
+    }
+    
+    func resetAction () {
+        calText = ""
+    }
+    
+    func backspaceAction () {
+        if !calText.isEmpty {
+            calText.removeLast()
+        }
+    }
+    
+    func calculate() {
+        guard !calText.isEmpty else {
+            return
+        }
+        
+        let expression = NSExpression(format: calText)
+        if let value = expression.expressionValue(with: nil, context: nil) as? Double {
+            calText = String(value)
+        } else {
+            calText = "Invalid input"
+        }
+    }
     
 }
 
 struct ButtonView: View {
-    
+    @EnvironmentObject var shared: SharedData
     
     let calChar: String
     let buttonColor: Color
+    
     var body: some View {
         Button(calChar) {
-            print(calChar)
+            switch(calChar) {
+            case "=" : shared.calculate()
+            case "Del" : shared.backspaceAction()
+            case "C" : shared.resetAction()
+            default :shared.buttonAction(calChar: calChar)
+            }
         }
         .frame(width: 80, height: 100)
         .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
@@ -27,17 +63,18 @@ struct ButtonView: View {
 }
 
 struct FullTextAreaView: View {
-    @Binding var calText: String
+    @EnvironmentObject var shared: SharedData
+    
     var body: some View {
-        Text("\(calText)")
+        Text(shared.calText)
             .frame(width: 300, height: 70, alignment: .trailing)
             .padding(.horizontal, 22)
-        
             .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
     }
 }
 
 struct InputButtonAreaView: View {
+    
     let numberColor = Color.teal
     let operationColor = Color.green
     let actionColor  = Color.orange
@@ -74,11 +111,11 @@ struct InputButtonAreaView: View {
                            buttonColor: operationColor)
             }
             HStack {
-                ButtonView(calChar: "00",
+                ButtonView(calChar: ".",
                            buttonColor: numberColor)
                 ButtonView(calChar: "0",
                            buttonColor: numberColor)
-                ButtonView(calChar: ".",
+                ButtonView(calChar: "00",
                            buttonColor: numberColor)
                 ButtonView(calChar: "/",
                            buttonColor: operationColor)
@@ -86,7 +123,7 @@ struct InputButtonAreaView: View {
             HStack {
                 ButtonView(calChar: "C",
                            buttonColor: actionColor)
-                ButtonView(calChar: "DEL",
+                ButtonView(calChar: "Del",
                            buttonColor: actionColor)
                 ButtonView(calChar: "=",
                            buttonColor: actionColor)
@@ -98,15 +135,16 @@ struct InputButtonAreaView: View {
 }
 
 struct ContentView: View {
-    @State private var calText:String = "000"
+    @StateObject var shared = SharedData()
     
     var body: some View {
         VStack {
-            FullTextAreaView(calText: $calText)
+            FullTextAreaView()
             InputButtonAreaView()
         }
         .padding()
         .font(.largeTitle)
+        .environmentObject(shared)
     }
 }
 
