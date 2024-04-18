b8979452d7ef0041f05cc16a1ca26575e03c6698
ksiomng0929 <nadana0929@gmail.com>
2024-04-18 15:38:05 +0900

    미완성 -> 계산 부분 구현 X

--- LAB7-Calculator/LAB7-Calculator/ContentView.swift
+++ LAB7-Calculator/LAB7-Calculator/ContentView.swift
@@ -1,24 +1,67 @@
-//
-//  ContentView.swift
-//  LAB7-Calculator
-//
-//  Created by Chung Wussup on 4/18/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State var print: String = ""
+    @State var input: [String] = []
+    let arr: [[String]] = [["7", "8", "9", "/"], ["4", "5", "6", "*"], ["1", "2", "3", "-"],[".", "0", "C", "+"], ["="]]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("0", text: $print)
+                .multilineTextAlignment(.trailing)
+                .frame(width: .infinity, height: 150, alignment: .trailing)
+                .font(.largeTitle)
+                .padding()
+            
+            
+            ForEach(arr, id: \.self) { row in
+                HStack(spacing: 10) {
+                    ForEach(row, id: \.self) { buttonLabel in
+                        Button(action: {
+                            ClickButton(buttonLabel)
+                        }) {
+                            Text(buttonLabel)
+                                .font(.title)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                                .background(Color.gray)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                        }
+                    }
+                }
+            }
         }
-        .padding()
+    }
+    func ClickButton(_ label: String) {
+        if label == "=" {
+            input.append(print)
+            print = ""
+            Swift.print(input)
+        } else if label == "C" {
+            print = ""
+        } else if label == "+" {
+            input.append(print)
+            input.append("+")
+            print = ""
+        } else if label == "-" {
+            input.append(print)
+            input.append("-")
+            print = ""
+        } else if label == "*" {
+            input.append(print)
+            input.append("*")
+            print = ""
+        } else if label == "/" {
+            input.append(print)
+            input.append("/")
+            print = ""
+        } else {
+            print += label
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
