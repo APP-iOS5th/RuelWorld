1d15d64da3031d124c5dc8a96ac863c162abae3d
song <nadana0929@gmail.com>
2024-04-25 21:28:57 +0900

    파일 재생성 ^^ ㅜ

--- TodoList2/TodoList2/Task.swift
+++ TodoList2/TodoList2/Task.swift
@@ -1,9 +1,13 @@
 //
 //  Task.swift
-//  TodoList
+//  TodoList2
 //
 //  Created by Song Kim on 4/25/24.
 //
 
 import Foundation
 
+struct Task {
+    var name: String
+    var isFinished: Bool
+}
---
+++ TodoList2/TodoList2/TodoList2App.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList2App.swift
+//  TodoList2
+//
+//  Created by Song Kim on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoList2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
