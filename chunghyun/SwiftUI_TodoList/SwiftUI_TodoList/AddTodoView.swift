338630f9e7c6e522562cdd2e0bd6ccd606883307
Ruel <c0407h@gmail.com>
2024-04-25 15:19:14 +0900

    내용 없을 시 AlertView 추가

--- chunghyun/SwiftUI_TodoList/SwiftUI_TodoList/AddTodoView.swift
+++ chunghyun/SwiftUI_TodoList/SwiftUI_TodoList/AddTodoView.swift
@@ -10,19 +10,20 @@ import SwiftUI
 struct AddTodoView: View {
     @Binding var isPresented: Bool
     @Environment(\.modelContext) var modelContext
-
+    
     @State var priority: Priority = .low
     
     @State var todo: String = ""
+    @State var isAlert: Bool = false
     
     var body: some View {
         VStack{
             Text("Todo 등록")
                 .padding()
-    
+            
             HStack {
                 TextField("내용을 입력하세요", text: $todo)
-  
+                
                 Menu("우선순위") {
                     Button("High") {
                         priority = .high
@@ -35,7 +36,7 @@ struct AddTodoView: View {
                     Button("Low") {
                         priority = .low
                     }
-
+                    
                 }
                 .foregroundStyle(priorityTextColor(priority))
                 
@@ -50,7 +51,14 @@ struct AddTodoView: View {
                 Text("등록")
             }
             
+            
+            
             Spacer()
+        }
+        .alert("내용이 없습니다.",isPresented: $isAlert) {
+            Button("OK", role: .cancel) {
+                isAlert.toggle()
+            }
         }
     }
     
@@ -67,9 +75,13 @@ struct AddTodoView: View {
     }
     
     func addTodo() {
-        let todo = Todo(completed: false, descrip: todo, priority: priority)
-        modelContext.insert(todo)
-        self.isPresented = false
+        if todo.isEmpty {
+            isAlert.toggle()
+        } else {
+            let todo = Todo(completed: false, descrip: todo, priority: priority)
+            modelContext.insert(todo)
+            self.isPresented = false
+        }
     }
     
 }
